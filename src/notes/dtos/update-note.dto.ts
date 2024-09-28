79704e96177a5f920e1b7c6d44dd386b61ef3f56
jonascsilva <55038425+jonascsilva@users.noreply.github.com>
2024-09-28 16:33:38 -0300

    add all basic notes routes

---
+++ src/notes/dtos/update-note.dto.ts
@@ -0,0 +1,15 @@
+import { IsString, IsOptional } from 'class-validator'
+
+export class UpdateNoteDto {
+  @IsString()
+  @IsOptional()
+  title: string
+
+  @IsString()
+  @IsOptional()
+  subject: string
+
+  @IsString()
+  @IsOptional()
+  content: string
+}
--- src/notes/notes.controller.ts
+++ src/notes/notes.controller.ts
@@ -1,19 +1,56 @@
-import { Controller, Get, Param } from '@nestjs/common'
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Delete,
+  Patch,
+  Body,
+  NotFoundException
+} from '@nestjs/common'
 
+import { Serialize } from '$/interceptor/serialize.interceptor'
+import { CreateNoteDto } from '$/notes/dtos/create-note.dto'
+import { NoteDto } from '$/notes/dtos/note.dto'
+import { UpdateNoteDto } from '$/notes/dtos/update-note.dto'
 import { Note } from '$/notes/note.entity'
 import { NotesService } from '$/notes/notes.service'
 
 @Controller('notes')
+@Serialize(NoteDto)
 export class NotesController {
   constructor(private readonly notesService: NotesService) {}
 
+  @Post()
+  createNote(@Body() body: CreateNoteDto): Promise<Note> {
+    console.log(body)
+
+    return this.notesService.create(body)
+  }
+
   @Get()
   getAllNotes(): Promise<Note[]> {
-    return this.notesService.getAllNotes()
+    return this.notesService.find()
   }
 
   @Get('/:id')
-  getNote(@Param('id') id: string): Promise<Note> {
-    return this.notesService.getNote(id)
+  async findNote(@Param('id') id: string): Promise<Note> {
+    const note = await this.notesService.findOne(id)
+
+    if (!note) {
+      throw new NotFoundException('Note not found')
+    }
+
+    return note
+  }
+
+  @Patch('/:id')
+  updateNote(@Param('id') id: string, @Body() body: UpdateNoteDto): Promise<Note> {
+    return this.notesService.update(id, body)
+  }
+
+  @Delete('/:id')
+  removeNote(@Param('id') id: string): Promise<Note> {
+    return this.notesService.remove(id)
   }
 }
