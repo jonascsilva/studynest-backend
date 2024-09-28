0ef8924791f6e42e9724436a337b7f88039b7c63
jonascsilva <55038425+jonascsilva@users.noreply.github.com>
2024-09-28 15:21:13 -0300

    add all basic users routes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,61 @@
-import { Body, Controller, Post } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+  Query
+} from '@nestjs/common'
 
+import { Serialize } from '$/interceptor/serialize.interceptor'
+import { AuthService } from '$/users/auth.service'
 import { CreateUserDto } from '$/users/dtos/create-user.dto'
+import { UpdateUserDto } from '$/users/dtos/update-user.dto'
+import { UserDto } from '$/users/dtos/user.dto'
 import { UsersService } from '$/users/users.service'
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private authService: AuthService
+  ) {}
 
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
-    this.usersService.create(body.email, body.password)
+    return this.authService.signup(body.email, body.password)
+  }
+
+  @Post('/signin')
+  signin(@Body() body: CreateUserDto) {
+    return this.authService.signin(body.email, body.password)
+  }
+
+  @Get('/:id')
+  async findUser(@Param('id') id: string) {
+    const user = await this.usersService.findOne(id)
+    if (!user) {
+      throw new NotFoundException('user not found')
+    }
+    return user
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.usersService.find(email)
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.remove(id)
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(id, body)
   }
 }
