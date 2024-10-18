baa46714771714d3a7b04be64afc72f1a1e80af8
jonascsilva <55038425+jonascsilva@users.noreply.github.com>
2024-10-18 16:20:52 +0000

    add google ai

---
+++ src/flashcards/ai.service.ts
@@ -0,0 +1,45 @@
+import { GoogleGenerativeAI, SchemaType } from '@google/generative-ai'
+import { Injectable } from '@nestjs/common'
+
+import { Flashcard } from '$/flashcards/flashcard.entity'
+
+const schema = {
+  type: SchemaType.OBJECT,
+  properties: {
+    question: {
+      type: SchemaType.STRING
+    },
+    answer: {
+      type: SchemaType.STRING
+    },
+    subject: {
+      type: SchemaType.STRING
+    }
+  },
+  required: ['question', 'answer', 'subject']
+}
+
+@Injectable()
+export class AiService {
+  constructor() {}
+
+  async generate(): Promise<Partial<Flashcard>> {
+    const genAI = new GoogleGenerativeAI('AIzaSyBfmHBXgj5tOkVjJQ7Si3p7E4sTLRJoBN8')
+
+    const model = genAI.getGenerativeModel({
+      model: 'gemini-1.5-flash',
+      generationConfig: {
+        responseMimeType: 'application/json',
+        responseSchema: schema
+      }
+    })
+
+    const prompt = 'Crie um flashcard (e somente um) sobre algum conteúdo do ensino médio'
+
+    const result = await model.generateContent(prompt)
+
+    const json = JSON.parse(result.response.text())
+
+    return json
+  }
+}
