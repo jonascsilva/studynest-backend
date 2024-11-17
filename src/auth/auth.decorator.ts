f150dd64445cd8da53ad3f3fcad1ed64ea1fc45f
jonascsilva <55038425+jonascsilva@users.noreply.github.com>
2024-11-17 00:40:53 +0000

    add swagger

--- src/auth/auth.decorator.ts
+++ src/auth/auth.decorator.ts
@@ -1,7 +1,8 @@
 import { applyDecorators, UseGuards } from '@nestjs/common'
+import { ApiBearerAuth } from '@nestjs/swagger'
 
 import { JwtAuthGuard } from '$/auth/jwt-auth.guard'
 
 export function Authenticated() {
-  return applyDecorators(UseGuards(JwtAuthGuard))
+  return applyDecorators(ApiBearerAuth(), UseGuards(JwtAuthGuard))
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 
 import { AppModule } from '$/app.module'
 
@@ -21,6 +22,16 @@ async function bootstrap() {
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
     credentials: true
   })
+
+  const config = new DocumentBuilder()
+    .setTitle('StudyNest API')
+    .setVersion('0.3.0')
+    .addBearerAuth()
+    .build()
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config)
+
+  SwaggerModule.setup('api', app, documentFactory)
 
   const port = configService.get('PORT')
 
