3f28460077aae538527bb0903a746e0251bb9363
jonascsilva <55038425+jonascsilva@users.noreply.github.com>
2024-11-27 01:54:56 +0000

    minor improvements

--- src/settings/dtos/update-user-settings.dto.ts
+++ src/settings/dtos/update-user-settings.dto.ts
@@ -1,14 +1,20 @@
-import { IsOptional, IsInt } from 'class-validator'
+import { IsOptional, IsInt, Min, Max } from 'class-validator'
 
 export class UpdateUserSettingsDto {
+  @Min(2)
+  @Max(6)
   @IsInt()
   @IsOptional()
   intervalsQuantity?: number
 
+  @Min(1)
+  @Max(6)
   @IsInt()
   @IsOptional()
   baseInterval?: number
 
+  @Min(1)
+  @Max(4)
   @IsInt()
   @IsOptional()
   intervalIncreaseRate?: number
